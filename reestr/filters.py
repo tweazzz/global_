b54421ab3264d81ffb335f37fd6c4171cbece15d
tweazzz <180103313@stu.sdu.edu.kz>
2025-08-20 23:44:47 +0500

    фильтр испольнитель

--- reestr/filters.py
+++ reestr/filters.py
@@ -1,4 +1,5 @@
 import django_filters
+from django.db.models import Q
 from .models import Reestr
 from auth_user.models import User
 
@@ -14,13 +15,16 @@ class ReestrFilter(django_filters.FilterSet):
     is_offsite = django_filters.CharFilter(lookup_expr='icontains')
     iin_bin = django_filters.CharFilter(lookup_expr='icontains')
 
-    # Фильтр по исполнителю:
-    # - ?executor=Iv         -> поиск по executor.username (icontains)
-    # - ?executor_id=12      -> поиск по PK исполнителя
-    executor = django_filters.CharFilter(field_name='executor__username', lookup_expr='icontains', label='Исполнитель (по username)')
+    # Универсальный фильтр исполнителя: ?executor=Iv -> ищет по username или full_name
+    executor = django_filters.CharFilter(method='filter_executor', label='Исполнитель (username или full_name)')
+
+    # Совместимость с запросом ?executor__full_name=... (если фронт посылает именно такое имя параметра)
+    executor__full_name = django_filters.CharFilter(field_name='executor__full_name', lookup_expr='icontains', label='Исполнитель (full_name legacy)')
+
+    # По id: ?executor_id=12
     executor_id = django_filters.ModelChoiceFilter(field_name='executor', queryset=User.objects.all(), label='Исполнитель (по id)')
 
-    # Фильтр по департаменту (как было)
+    # Фильтр по департаменту
     department__dep_name = django_filters.CharFilter(field_name='department__dep_name', lookup_expr='icontains')
 
     # Фильтр по компании (подстрока)
@@ -35,7 +39,19 @@ class ReestrFilter(django_filters.FilterSet):
         fields = [
             'customer_name', 'payer', 'object_name', 'object_address',
             'contract_number', 'bank_name', 'title_number', 'is_offsite',
-            'iin_bin', 'executor', 'executor_id', 'department__dep_name',
+            'iin_bin', 'executor', 'executor_id', 'executor__full_name', 'department__dep_name',
             'company',
             'contract_start_date', 'contract_end_date'
         ]
+
+    def filter_executor(self, queryset, name, value):
+        """
+        Ищем по executor.username ИЛИ по executor.full_name (icontains).
+        Если хотите, можно расширить на email и т.д.
+        """
+        if not value:
+            return queryset
+        return queryset.filter(
+            Q(executor__username__icontains=value) |
+            Q(executor__full_name__icontains=value)
+        )
